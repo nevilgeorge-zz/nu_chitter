1b0c6a5868725de1e94499fe3dcf8ff65b8195e9
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-04-06 14:34:09 -0500

    working with goroutines

--- chitter.go
+++ chitter.go
@@ -14,18 +14,36 @@ import (
 // client struct to keep track of channels and id of each client
 type Client struct {
 	id int
-	message chan string
+	incoming chan string
+	outgoing chan string
 	conn net.Conn
 }
 
-func (client *Client) Write(msg string) {
-	client.conn.Write([]byte(msg))
+// methods for the Client struct
+func (client *Client) Read() {
+	conn := client.conn
+	for {
+		buffer := make([]byte, 1024)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			conn.Close()
+			break
+		}
+		input := string(buffer)
+		client.incoming <- input
+	}
+}
+
+func (client *Client) Write() {
+	for data := range client.outgoing {
+		client.conn.Write([]byte(data))
+	}
 }
 
 func (client *Client) PrintId() {
 	t := strconv.Itoa(client.id)
 	t += "\n"
-	client.conn.Write([]byte(t))
+	client.outgoing <- t
 }
 
 // array to keep track of clients currently connected to the server
@@ -54,63 +72,60 @@ func main() {
 			fmt.Println("Error occurred in accepting a new connection: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("New connection!")
 		// no error, handle the new connection
 		count += 1
+		fmt.Println("New connection! Id: " + strconv.Itoa(count))
+		// create a new Client instance
 		newClient := new(Client)
 		newClient.id = count
-		newClient.message = make(chan string)
+		newClient.incoming = make(chan string)
+		newClient.outgoing = make(chan string)
 		newClient.conn = conn
 		clients = append(clients, newClient)
-		fmt.Println(len(clients))
 		go handleRequest(conn, *newClient)
 	}
 
 }
 
 func handleRequest(conn net.Conn, cli Client) {
+	// create separate go routines to read from and write to the client
+	go cli.Read()
+	go cli.Write()
+	// run the select statement in the current go routine
 	for {
-		buffer := make([]byte, 1024)
-		_, err := conn.Read(buffer)
-		if err != nil {
-			conn.Close()
-			break
-		}
-		input := string(buffer)
-		if input[0:6] == "whoami" {
-			cli.PrintId()
-		} else if input[0:3] == "all" {
-			text := grabTextAfterColon(input)
-			broadcastMsg(text)
-		} else {
-			id, err := strconv.Atoi(string(input[0]))
-			if err != nil {
-				continue
-			}
-			text := grabTextAfterColon(input)
-			sendToRecipient(id, text)
-		}
-		
-		// broadcast <- string(buffer)
 		select {
-		case personalMsg := <-cli.message:
-			conn.Write([]byte(personalMsg))
-		case broadcastMsg := <-broadcast:
-			conn.Write([]byte(broadcastMsg))
-		default:
-			// we need to have a default otherwise the select statement waits
-			// just continue iterating through the for loop
-			continue
+			// when a incoming message is present, we need to parse it and then handle it correctly
+		case msgIn := <-cli.incoming:
+			if msgIn[0:6] == "whoami" {
+				// print id
+				cli.PrintId()
+			} else if msgIn[0:3] == "all" {
+				// grab message text and broadcast to all connected clients
+				text := grabTextAfterColon(msgIn)
+				broadcastMsg(text)
+			} else {
+				// get id of receiver, grab message text and then send the message to the recipient
+				id, err := strconv.Atoi(string(msgIn[0]))
+				if err != nil || id > len(clients) {
+					continue
+				}
+				text := grabTextAfterColon(msgIn)
+				sendToRecipient(cli.id, id, text)
+			}
 		}
 	}
 }
 
+// function that sends a message to all connected clients
+// params: msg - string
 func broadcastMsg(msg string) {
 	for x := 0; x < len(clients); x++ {
-		clients[x].Write(msg)
+		clients[x].outgoing <- msg
 	}
 }
 
+// function that parses the string and grabs the message being sent
+// params: input - string, return: text - string
 func grabTextAfterColon(input string) (text string) {
 	index := strings.Index(input, ":")
 	if input[index + 1] == ' ' {
@@ -121,7 +136,9 @@ func grabTextAfterColon(input string) (text string) {
 	return text
 }
 
-func sendToRecipient(id int, text string) {
-	curr := clients[id - 1]
-	curr.Write(text)
+// function that handles sending a private message 
+// params: sender id  - int, receiver id - int, text - string
+func sendToRecipient(sender int, receiver int, text string) {
+	message := strconv.Itoa(sender) + " : " + text
+	clients[receiver - 1].outgoing <- message
 }
